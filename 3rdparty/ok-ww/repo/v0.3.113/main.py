9bfc84ab3dcf1ede08194617a6334016296fd144
kuihuaxin <2891920749@qq.com>
2024-11-01 21:33:36 +0800

    搞错了第三方不应该放这

--- 3rdparty/ok-ww/repo/v0.3.113/main.py
+++
@@ -1,6 +0,0 @@
-from config import config
-from ok.OK import OK
-
-config = config
-ok = OK(config)
-ok.start()
--- 3rdparty/ok-ww/repo/v0.3.113/main_gpu.py
+++
@@ -1,7 +0,0 @@
-from config import config
-from ok.OK import OK
-
-config = config
-config['ocr']['lib'] = 'paddleocr'
-ok = OK(config)
-ok.start()
